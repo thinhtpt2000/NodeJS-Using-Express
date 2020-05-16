c651b047d6f5458377567552a93168b1f85986ae
thinhtpt2000 <tthinh2128@gmail.com>
2020-05-16 10:48:56 +0700

    add middle ware

---
+++ 13-middleware/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var userRoute = require('./routes/user.route');
+
+var app = express();
+var port = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+	res.render('index', {
+		name: 'ThinhTPT'
+	});
+});
+
+app.use('/users', userRoute);
+
+app.listen(port, () => {
+	console.log('Example app listening on port ' + port);
+});
